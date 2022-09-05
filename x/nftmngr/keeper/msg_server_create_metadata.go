d357dd37855fc80699f1cea3504048b761e72a90
jonathan-six <97500179+jonathan-six@users.noreply.github.com>
2022-09-05 16:08:12 +0700

    changed camel case to snake case

--- x/nftmngr/keeper/msg_server_create_metadata.go
+++ x/nftmngr/keeper/msg_server_create_metadata.go
@@ -2,16 +2,41 @@ package keeper
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"sixnft/x/nftmngr/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CreateMetadata(goCtx context.Context, msg *types.MsgCreateMetadata) (*types.MsgCreateMetadataResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	metadata, err := base64.StdEncoding.DecodeString(msg.Base64NFTData)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrParsingBase64, err.Error())
+	}
+	data := types.NftData{}
+	err = k.cdc.(*codec.ProtoCodec).UnmarshalJSON(metadata, &data)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrParsingSchemaMessage, err.Error())
+	}
 
-	return &types.MsgCreateMetadataResponse{}, nil
+	fmt.Println("####################### ", data.NftSchemaCode)
+
+	// Check if the data already exists
+	_, found := k.Keeper.GetNftData(ctx, data.NftSchemaCode, data.TokenId)
+	if found {
+		return nil, sdkerrors.Wrap(types.ErrSchemaAlreadyExists, data.NftSchemaCode)
+	}
+	// Add the data to the store
+	k.Keeper.SetNftData(ctx, data)
+
+	return &types.MsgCreateMetadataResponse{
+		NftSchemaCode: data.NftSchemaCode,
+		TokenId:       data.TokenId,
+	}, nil
 }
