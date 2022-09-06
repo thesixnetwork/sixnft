93b0afbaae6314cb0e9364cce687392e6738b27e
ngam <ngamwanasin.a@gmail.com>
2022-09-06 10:42:59 +0700

    add example

--- x/nftmngr/keeper/msg_server_test.go
+++ x/nftmngr/keeper/msg_server_test.go
@@ -9,8 +9,8 @@ import (
 	"sixnft/x/nftmngr/keeper"
 	"sixnft/x/nftmngr/types"
 
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/gogo/protobuf/jsonpb"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
@@ -21,7 +21,7 @@ func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 func TestCreateData(t *testing.T) {
 	fmt.Println("Start Test")
 	// base64Data := "ewogICAgIm5mdF9zY2hlbWFfY29kZSI6ICJidWFrYXcxIiwKICAgICJ0b2tlbl9pZCI6ICIxIiwKICAgICJ0b2tlbl9vd25lciI6ICI2bmZ0MTlwNXl2d3h0YzJxbmdud2RxbGhmemRjZDBzc25jd3d1bXJ4MmphIiwKICAgICJvcmlnaW5faW1hZ2UiOiAiaHR0cHM6Ly9iazFuZnQuc2l4bmV0d29yay5pby9pcGZzL1FtYUJVZHFUY3RWdGVhanBvTVZFcG81NVhFWGUyWVZ3V0RnZU5pMVVUNDdVS1UvMS5wbmciLAogICAgIm9yaWdpbl9hdHRyaWJ1dGVzIjogWwogICAgICAgIHsKICAgICAgICAgICAgIm5hbWUiOiAiYmFja2dyb3VuZF9sIiwKICAgICAgICAgICAgInZhbHVlIjogIkwgQkcgUmluZ3NpZGUgR29sZGVuIgogICAgICAgIH0sCiAgICAgICAgewogICAgICAgICAgICAibmFtZSI6ICJiYWNrZ3JvdW5kX3IiLAogICAgICAgICAgICAidmFsdWUiOiAiUiBUaWdlciIKICAgICAgICB9LAogICAgICAgIHsKICAgICAgICAgICAgIm5hbWUiOiAicGxhdGVfbCIsCiAgICAgICAgICAgICJ2YWx1ZSI6ICJMIEJhbmNoYW1layBCbGFjayIKICAgICAgICB9LAogICAgICAgIHsKICAgICAgICAgICAgIm5hbWUiOiAicGxhdGVfciIsCiAgICAgICAgICAgICJ2YWx1ZSI6ICJSIEJhbmNoYW1layBHb2xkZW4iCiAgICAgICAgfSwKICAgICAgICB7CiAgICAgICAgICAgICJuYW1lIjogImJvZHlfbCIsCiAgICAgICAgICAgICJ2YWx1ZSI6ICJMIEJvZHkgTm9ybWFsIEdlbnRsZW1hbiIKICAgICAgICB9LAogICAgICAgIHsKICAgICAgICAgICAgIm5hbWUiOiAiaGVhZF9sIiwKICAgICAgICAgICAgInZhbHVlIjogIkwgTk9OIEhFQUQiCiAgICAgICAgfSwKICAgICAgICB7CiAgICAgICAgICAgICJuYW1lIjogImNsb3RoZXNfbCIsCiAgICAgICAgICAgICJ2YWx1ZSI6ICJMIFJvYmUgR29sZGVuIgogICAgICAgIH0sCiAgICAgICAgewogICAgICAgICAgICAibmFtZSI6ICJleHRyYV9sIiwKICAgICAgICAgICAgInZhbHVlIjogIkwgTk8gRVhUUkEiCiAgICAgICAgfSwKICAgICAgICB7CiAgICAgICAgICAgICJuYW1lIjogImhhbmRfbCIsCiAgICAgICAgICAgICJ2YWx1ZSI6ICJMIEdsb3ZlIEJsYWNrIEJWIgogICAgICAgIH0sCiAgICAgICAgewogICAgICAgICAgICAibmFtZSI6ICJpbmZsdWVuY2VyIiwKICAgICAgICAgICAgInZhbHVlIjogIlRlZXJhd2F0IFlpb3lpbSIKICAgICAgICB9CiAgICBdLAogICAgIm9uY2hhaW5fYXR0cmlidXRlcyI6IFsKICAgICAgICB7CiAgICAgICAgICAgICJuYW1lIjogImV4Y2x1c2l2ZV9wYXJ0eV9hY2Nlc3MiLAogICAgICAgICAgICAidmFsdWUiOiBmYWxzZQogICAgICAgIH0sCiAgICAgICAgewogICAgICAgICAgICAibmFtZSI6ICJmaXJzdF9kaXNjb3VudF91c2VkIiwKICAgICAgICAgICAgInZhbHVlIjogZmFsc2UKICAgICAgICB9CiAgICBdCn0K"
-	k, _ := keepertest.NftmngrKeeper(t)
+	// k, _ := keepertest.NftmngrKeeper(t)
 	// metadata, err := base64.StdEncoding.DecodeString(base64Data)
 	// if err != nil {
 	// 	fmt.Println(err.Error())
@@ -33,22 +33,82 @@ func TestCreateData(t *testing.T) {
 		"token_id": "1",
 		"token_owner": "6nft19p5yvwxtc2qngnwdqlhfzdcd0ssncwwumrx2ja",
 		"origin_image": "https://bk1nft.sixnetwork.io/ipfs/QmaBUdqTctVteajpoMVEpo55XEXe2YVwWDgeNi1UT47UKU/1.png",
-		
+		"origin_attributes": [
+			{
+				"name": "background_l",
+				"string_attribute_value" : {"value": "L BG Ringside Golden"}
+			},
+			{
+				"name": "background_r",
+				"string_attribute_value" : {"value": "R Tiger"}
+			},
+			{
+				"name": "plate_l",
+				"string_attribute_value" : {"value": "L Banchamek Black"}
+			},
+			{
+				"name": "plate_r",
+				"string_attribute_value" : {"value": "R Banchamek Golden"}
+			},
+			{
+				"name": "body_l",
+				"string_attribute_value" : {"value": "L Body Normal Gentleman"}
+			},
+			{
+				"name": "head_l",
+				"string_attribute_value" : {"value": "L NON HEAD"}
+			},
+			{
+				"name": "clothes_l",
+				"string_attribute_value" : {"value": "L Robe Golden"}
+			},
+			{
+				"name": "extra_l",
+				"string_attribute_value" : {"value": "L NO EXTRA"}
+			},
+			{
+				"name": "hand_l",
+				"string_attribute_value" : {"value": "L Glove Black BV"}
+			},
+			{
+				"name": "influencer",
+				"string_attribute_value" : {"value": "Teerawat Yioyim"}
+			}
+		],
 		"onchain_attributes": [
 			{
 				"name": "exclusive_party_access",
-				"value": {
+				"boolean_attribute_value": {
+					"value": false
+				}
+			},
+			{
+				"name": "first_discount_used",
+				"boolean_attribute_value": {
 					"value": false
 				}
 			}
 		]
 	}
+	
 	`
 	data := types.NftData{}
-	err := k.GetCodec().(*codec.ProtoCodec).UnmarshalJSON([]byte(metadata), &data)
+	// err := k.GetCodec().(*codec.ProtoCodec).UnmarshalJSON([]byte(metadata), &data)
+	err := jsonpb.UnmarshalString(metadata, &data)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 	fmt.Println("DataOutput", data)
+
+	// data := types.NftData{}
+	// attribute := types.NftAttributeValue{
+	// 	Name: "exclusive_party_access",
+	// }
+	// attribute.Value = &types.NftAttributeValue_BooleanAttributeValue_{BooleanAttributeValue: &types.NftAttributeValue_BooleanAttributeValue{Value: true}}
+	// data.OnchainAttributes = append(data.OnchainAttributes, &attribute)
+
+	// m := jsonpb.Marshaler{}
+	// str, _ := m.MarshalToString(&attribute)
+	// fmt.Println(str)
 }
