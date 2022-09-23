a73730ac7a82d926c72b6d4ed31d1eb95c514598
dDee-D-six <dee.hamdee.084@gmail.com>
2022-09-23 17:07:19 +0700

    remove vue and ts-client

--- ts-client/sixnft.evmsupport/types.ts
+++
@@ -1,9 +0,0 @@
-import { AddressBinding } from "./types/evmsupport/address_binding"
-import { Params } from "./types/evmsupport/params"
-
-
-export {     
-    AddressBinding,
-    Params,
-    
- }
