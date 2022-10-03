d24235e95111e1f15b42f72e54391ee8906c6445
jonathan-six <97500179+jonathan-six@users.noreply.github.com>
2022-10-03 11:46:48 +0700

    started paginate (not done)

--- x/nftmngr/keeper/grpc_query_nft_collection.go
+++ x/nftmngr/keeper/grpc_query_nft_collection.go
@@ -3,7 +3,9 @@ package keeper
 import (
 	"context"
 
+	"github.com/cosmos/cosmos-sdk/0.20220523154235-2921a1c3c918/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/thesixnetwork/sixnft/x/nftmngr/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -13,7 +15,22 @@ func (k Keeper) NftCollection(c context.Context, req *types.QueryGetNftCollectio
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
+
+	var nftCollections []types.NftCollection
 	ctx := sdk.UnwrapSDKContext(c)
+
+	store := ctx.KVStore(k.storeKey)
+	NftcollectionStore := prefix.NewStore(store, types.KeyPrefix(types.NftCollectionKeyPrefix))
+
+	pageRes, err := query.Paginate(NftcollectionStore, req.Pagination, func(key []byte, value []byte) error {
+		var nftCollection types.NftCollection
+		if err := k.cdc.Unmarshal(value, &nftCollection); err != nil {
+			return err
+		}
+
+		nftCollections = append(nftCollections, nftCollection)
+		return nil
+	})
 
 	val, found := k.GetNftCollection(
 		ctx,
@@ -23,5 +40,5 @@ func (k Keeper) NftCollection(c context.Context, req *types.QueryGetNftCollectio
 		return nil, status.Error(codes.NotFound, "not found")
 	}
 
-	return &types.QueryGetNftCollectionResponse{NftCollection: val}, nil
+	return &types.QueryGetNftCollectionResponse{NftCollection: val, Pagination: pageRes}, nil
 }
