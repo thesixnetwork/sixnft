3baf7056f6d5f7aeb5c81ebec87c139c5ec0dbe4
dDee-D-six <dee.hamdee.084@gmail.com>
2022-10-12 21:23:24 +0700

    update script

--- scripts/init_data.sh
+++ scripts/init_data.sh
@@ -1,10 +1,11 @@
 grantOracle()
 {
     echo "Grant 'oracle' to $1"
-    sixnftd tx admin grant-permission oracle $1 --from alice --gas auto --gas-adjustment 1.5 --gas-prices 0.1stake -y \
+    sixnftd tx nftadmin grant-permission oracle $1 --from alice --gas auto --gas-adjustment 1.5 --gas-prices 0.1stake -y \
         --node ${RPC_ENDPOINT}
 }
 
+RPC_ENDPOINT=http://localhost:26657
 BASE64_SCHEMA=`cat nft-schema.json | base64 | tr -d '\n'`
 
 sixnftd tx nftadmin grant-permission oracle_admin $(sixnftd keys show alice -a) --from alice -y --node ${RPC_ENDPOINT}
@@ -18,5 +19,5 @@ grantOracle 6nft1mvw6jk5wuhxmyd0edgj9d3d0nr5amrmxsejcc7
 grantOracle 6nft1d5auhw4hfg49fmzfngpaz98vn4f59yazu6j72s
 grantOracle 6nft1kl7qw9jw0zatph3jc7mdaf3xd5p7aw4edy8svq
 
-sixnftd q admin show-authorization \
+sixnftd q nftadmin show-authorization \
     --node ${RPC_ENDPOINT}
