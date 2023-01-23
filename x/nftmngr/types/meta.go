a87a2351836bf2e66a811a0d28da43b22034396d
dDee-D-six <dee.hamdee.084@gmail.com>
2023-01-23 18:12:23 +0700

    :sparkles: hidden overing while using action setdisplay attribute

--- x/nftmngr/types/meta.go
+++ x/nftmngr/types/meta.go
@@ -435,6 +435,9 @@ func (m *Metadata) SetDisplayArribute(key string, value string) error {
 	if attri == nil {
 		return sdkerrors.Wrap(ErrAttributeNotFoundForAction, key)
 	}
+	if !(attri.AttributeValue.HiddenOveride) {
+		return sdkerrors.Wrap(ErrAttributeOverriding, "The selected attribute is not allowed to be hidden")
+	}
 	if _bool := attri.AttributeValue.GetHiddenToMarketplace() == bool_val; _bool {
 		return nil
 	}
