287b5ec88edf80392e7a007dff2470319acba3cb
dDee-D-six <dee.hamdee.084@gmail.com>
2023-01-26 18:16:58 +0700

    ::green_heart: add query version v063

--- x/nftoracle/client/cli/query.go
+++ x/nftoracle/client/cli/query.go
@@ -29,6 +29,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowMintRequest())
 	cmd.AddCommand(CmdListActionRequest())
 	cmd.AddCommand(CmdShowActionRequest())
+	cmd.AddCommand(CmdListActionRequestV063())
+	cmd.AddCommand(CmdShowActionRequestV063())
 	cmd.AddCommand(CmdListCollectionOwnerRequest())
 	cmd.AddCommand(CmdShowCollectionOwnerRequest())
 	cmd.AddCommand(CmdShowOracleConfig())
